801857a93cdd56f262dc9ce80c658d2a1dcd8364
Rajarshi <impladimpla@gmail.com>
2024-10-18 16:29:54 +0530

    first commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Flutter</li>
+        <li>Android Jetpack</li>
+        <li>Firebase</li>
+        <li>Node.js</li>
+        <li>Next.js</li>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>DPS Numaligarh</li>
+        <li>Shiv Jyoti International School</li>
+        <li>KIIT University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Prompt Engineering for ChatGPT</li>
+        <li>Data Analysis with Python</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+           I am a full stack app and web developer. I enjoy creating seamless, user-friendly experiences. I have experience working with Kotlin, Dart, Javascript, React, next.js and python. I am a team player and I am excited to work with others to create amazing applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/ProjectsSection.jsx
@@ -0,0 +1,114 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+  {
+    id: 1,
+    title: "Full Stack E-Commerce App",
+    description: "A fully functional e-commerce app developed using Flutter and Firebase",
+    image: "/images/projects/1.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/sands21/ecommerce-app.git",
+  },
+  {
+    id: 2,
+    title: "Personal Portfolio Website",
+    description: "Portfolio website made using Next.js",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/sands21/youtube-clone.git",
+  },
+  {
+    id: 3,
+    title: "React based Youtube Clone",
+    description: "A fully functional youtube clone using  react.js and youtube data api",
+    image: "/images/projects/3.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/sands21/youtube-clone.git",
+  },
+  {
+    id: 4,
+    title: "Wishlist App",
+    description: "A wish list app made using jetpack compose and room database",
+    image: "/images/projects/4.webp",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/sands21/Wish-List-App.git",
+  },
+  {
+    id: 5,
+    title: "To-Do App",
+    description: "A simple to-do app made using Flutter",
+    image: "/images/projects/5.jpg",
+    tag: ["All", "Mobile"],
+    gitUrl: "https://github.com/sands21/Todo-app.git",
+  },
+];
+
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
+  return (
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default ProjectsSection;
